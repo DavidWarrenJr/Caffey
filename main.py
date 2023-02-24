b361d3eb5702986fe332e913fc5a9f047e354fbe
David <davidwarrenjr90@gmail.com>
2023-02-24 16:20:29 -0600

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
+
+db = SQLAlchemy(app)
+
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), nullable=False)
+    map_url = db.Column(db.String(250))
+    img_url = db.Column(db.String(250))
+    location = db.Column(db.String(250), nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    seats = db.Column(db.Integer, nullable=False)
+    coffee_price = db.Column(db.Float)
+
+    def to_dict(self):
+        return {column.name: getattr(self, column.name) for column in self.__table__.columns}
+
+
+@app.route("/")
+def home():
+    all_cafes = db.session.query(Cafe).all()
+    for cafe in all_cafes:
+        print(cafe.to_dict())
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
