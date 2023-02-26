7ffd9cde54a1f9ea3e3e845076e5a5d0b11f9084
David <davidwarrenjr90@gmail.com>
2023-02-26 09:50:36 -0600

    Dropdown remembers selection after refresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
+from operator import attrgetter
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cafes.db"
@@ -24,10 +25,23 @@ class Cafe(db.Model):
         return {column.name: getattr(self, column.name) for column in self.__table__.columns}
 
 
+def apply_filter(filter, cafes) -> list:
+    if not filter:
+        filter = "name"
+
+    cafes.sort(key=attrgetter(filter))
+    return cafes
+
+
 @app.route("/")
 def home():
     all_cafes = db.session.query(Cafe).all()
-    return render_template("index.html", cafes=all_cafes)
+
+    filter = request.args.get("filter")
+    filtered_cafes = apply_filter(filter=filter, cafes=all_cafes)
+
+
+    return render_template("index.html", cafes=all_cafes, selected_filter=filter)
 
 
 if __name__ == "__main__":
